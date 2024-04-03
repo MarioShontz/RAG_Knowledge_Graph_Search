54d70fd9c8517238f1d9c9debcd89158615075cf
MarioShontz <shontzm@gmail.com>
2024-04-03 19:29:50 -0400

    wip - scraping simple info, working on json output

--- ScrapingUtils.py
+++ ScrapingUtils.py
@@ -1,3 +1,5 @@
+import os
+from sys import argv
 import configparser
 from urllib.parse import urlparse, urljoin
 
@@ -7,75 +9,98 @@ from scrapy.selector import Selector
 
 
 class ScrapingUtils:
-    class FsuSpider(scrapy.Spider):
-        name = "fsu_spider"
-        def __init__(self, start_url, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.start_urls = [start_url]
+    def __init__(self,config):
+        self.config = config
 
-        def parse(self, response):
-            # Extract all links, metadata, and text
-            links = response.css('a::attr(href)').getall()
-            metadata = response.xpath('//meta').getall()
-            text = response.xpath("//body//text()").getall()
+    def create_spider_class(self):
+        class Spider(scrapy.Spider):
+            name = "spider"
 
-            # Placeholder for user-defined logic with scraped data
-            self.logic_on_scrape(response.url, links, metadata, text)
+            allowed_domains = start_urls = self.get_domains() # need to fix this to only allow one domain at a time (keeping website information modular)
+            sibling_or_parent_domain_depth = self.config["sibling_or_parent_domain_depth"]
+            out_of_domain_depth = self.config["out_of_domain_depth"]
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
 
-            # For crawling all pages within the domain
-            for link in links:
-                absolute_url = urljoin(response.url, link)
-                yield response.follow(absolute_url, callback=self.parse)
+            def scrape_webpage(self,url):
+                process = CrawlerProcess()
+                process.crawl(Spider, start_url=url)
+                process.start()
 
-        def logic_on_scrape(self, url, links, metadata, text):
-            # User-defined logic goes here
-            pass
+            def scrape_domains(self):
+                for start_url in self.start_urls:
+                    self.scrape_webpage(start_url)
 
-    def scrape_single_page(self, webpage_url):
-        process = CrawlerProcess()
-        process.crawl(self.FsuSpider, start_url=webpage_url)
-        process.start()
+            def parse(self, response):
+                # Extract all links, metadata, and text
+                links = response.css('a::attr(href)').getall()
+                metadata = response.xpath('//meta').getall()
+                text = response.xpath("//body//text()").getall()
+                all_elements = response.xpath(
+        "//*[not(contains(@style, 'display: none')) and not(ancestor::*[contains(@style, 'display: none')])]"
+    )
+                # handle scraped data
+                self.logic_on_scrape(response.url, links, metadata, text)
+                # self.logic_on_scrape(all_elements)
 
-    def scrape_domain(self, website_url):
-        self.scrape_single_page(website_url)
+                # For crawling all pages within the domain
+                for link in links:
+                    absolute_url = urljoin(response.url, link)
+                    yield response.follow(absolute_url, callback=self.parse)
 
-    def scrape_sitemap(self, homepage_url):
-        # This method would require additional logic to fetch and parse sitemap.xml,
-        # and then use Scrapy requests to scrape each URL found in the sitemap.
-        pass
-
-    def scrape_with_fallback(self, homepage_url):
-        # This method should attempt to scrape using sitemap first,
-        # and if that fails, fallback to crawling the domain starting from the homepage.
-        pass
-
-    def read_and_filter_sites(filename):
-        valid_sites = set()
-        with open(filename, 'r') as site_list:
-            for line in site_list:
-                # Strip leading and trailing whitespace and ignore comments
+            def logic_on_scrape(self, url, links, metadata, text):
+                # User-defined logic goes here
+                output = {"url": url, "links": links, "metadata": metadata, "text": text}
+                with open("output.json", "a") as f:
+                    pass
+                        
+            
+            # def logic_on_scrape(self, body):
+            #     with open("output.txt", "a") as f:
+            #         for element in body:
+            #             f.write(element.get() + "\n")
+            
+        return Spider
+    
+    def get_domains(self):
+        domains = []
+        domains_file = self.config["domains_file"]
+        # Open the file for reading
+        with open(domains_file, 'r') as domains_list:
+            # Iterate over each line in the file
+            for line in domains_list:
+                # Strip whitespace and newline characters, then add the domain to the list
                 line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                
-                # Remove all whitespace from the line
-                line = ''.join(line.split())
-                
-                # Check if the line has a valid scheme (http or https)
-                if urlparse(line).scheme:
-                    valid_sites.add(line)
+                # add if not blank or a comment
+                if len(line) > 0 and not line.startswith("#"):
+                    # Ensure the line has a scheme
+                    if not line.startswith(('http://', 'https://')):
+                        line = 'http://' + line
+                    domains.append(line)
+        return domains
 
-        return valid_sites
 
 if __name__ == "__main__":
+    if len(argv) < 2:
+        LEVEL = "--test"
+    elif len(argv) == 2:
+        LEVEL = str(argv[1]) #--test or --prod
+    else:
+        raise Exception("Invalid number of arguments. Please provide only one argument (--test or --prod)")
+    assert LEVEL in ["--test", "--prod"], "Invalid level. Please use --test or --prod"
+    config_level = "TESTING" if LEVEL == "--test" else "PRODUCTION"
+
     # read config file and get domains filename
     config = configparser.ConfigParser()
     config.read('config.ini')
-    domains = config['TESTING']['domain_list_file']
+    config = config[config_level]
 
-    utils = ScrapingUtils()
-    #get list of domains
-    sites = utils.read_and_filter_sites(domains)
-    
-    #Run scraper on each site
-    utils.scrape_domain(domains)
+    SU = ScrapingUtils(config)
+    SpiderClass = SU.create_spider_class()
+
+    FSUSpider=SpiderClass()
+    FSUSpider.scrape_webpage("fsu.edu")
+    # FSUSpider.scrape_domains()
+    # utils = ScrapingUtils()
+    # #get list of domains
+    # sites = utils.read_and_filter_sites(domains)
