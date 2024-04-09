6918b6fecc1f3abceb7aaa47db280cb752c7a626
MarioShontz <shontzm@gmail.com>
2024-04-09 16:16:10 -0400

    Knowledge Graph Retrieval working - Needs higher quality data

--- Spiders/GFMDSpider.py
+++ Spiders/GFMDSpider.py
@@ -2,6 +2,8 @@ from urllib.parse import urlparse
 # import json
 import subprocess
 import html
+import re
+
 # from sys import argv
 # import pathlib
 # import configparser
@@ -12,122 +14,123 @@ from scrapy.spiders import Rule
 
 from Spider import Spider
 
+
 class GFMDSpider(Spider):
     name = "GFMDSpider"
-    start_afresh = False
-    rules = ( 
-        Rule(
-            LinkExtractor(
-                allow=(''),
-                deny=(
-                    '\\*.js',
-                    '\\*.ico',
-                    '\\?\\w\\.html'
-                    '\\*\\.pdf',
-                    '\\*\\.jpg',
-                    '\\*\\.png',
-                    '\\*\\.gif',
-                    '\\*\\.css',
-                    '\\*\\.js',
-                    '\\*\\.svg',
-                    '\\*\\.jpeg',
-                    '\\*\\.zip',
-                    '\\*\\.ppt',
-                    '\\*\\.pptx',
-                    '\\*\\.xls',
-                    '\\*\\.xlsx',
-                    '\\*\\.doc',
-                    '\\*\\.docx',
-                    '\\*\\.txt',
-                    '\\*\\.xml',
-                    '\\*\\.json',
-                    '\\*\\.rss',
-                    '\\*\\.atom',
-                    '\\*\\.rdf',
-                    '\\*\\.xml',
-                    '\\*\\.do',
-                    '\\*\\.db',
-                    '\\*redirect',
-                ),
-            ),
-        )
-    )
+    start_afresh = True #takes precedence over update and retry settings below
+    
+    update_links = True
+    update_content = False
+    retry_failed_content = True
+
     def __init__(self, start_url):
         super().__init__(start_url=start_url)
+        self.start_page_links = []
 
-    def unmangle_utf8(self, text):
+    def unmangle_utf8(self, text: str):
         decoded_text = html.unescape(text)
-        replace_dict = {
-            "\u00e2\u20ac\u2122": "'",  # Replace with apostrophe
-            "\u00e2\u20ac\u201d": "—", # Replace with em dash
-            "\u00c3\u00bak\u00c3\u00ae": "ú",  # Replace with ú
-            "\u00c3\u00b3" : "ó",  # Replace with ó
-            "\u00c3\u00ae" : "î",  # Replace with î
-            "\u00c2\u00a0" : "", # delete all non-breaking spaces
-            "\u000b" : "", # delete all vertical tabs
-            "\u00e2\u20ac\u2039" : "",
-            "\u00c3\u00af" : "ï",
-            " \\[\u2026\\]" : "", 
-            "\u00e2\u20ac\u201c" : "—",       
-        }
-        for key, value in replace_dict.items():
-            decoded_text = decoded_text.replace(key, value)
+        # replace_dict = {
+        #     "\u00e2\u20ac\u2122": "\'",  # Replace with apostrophe
+        #     "\u00e2\u20ac\u201d": "—", # Replace with em dash
+        #     "\u00c3\u00bak\u00c3\u00ae": "ú",  # Replace with ú
+        #     "\u00c3\u00b3" : "ó",  # Replace with ó
+        #     "\u00c3\u00ae" : "î",  # Replace with î
+        #     "\u00c2\u00a0" : "", # delete all non-breaking spaces
+        #     "\u000b" : "", # delete all vertical tabs
+        #     "\u00e2\u20ac\u2039" : "",
+        #     "\u00c3\u00af" : "ï",
+        #     " \\[\u2026\\]" : "",
+        #     "\u00e2\u20ac\u201c" : "—",
+        #     '\u2014',: "—",
+        #     '\u2019' : "\'",
+        #     '\u201c' : '\"',
+        #     '\u201d' : '\"',
+        # }
+        # for key, value in replace_dict.items():
+        #     decoded_text = decoded_text.replace(key, value)
         return decoded_text
-        
-    def run_website_to_md_js(self, url):
+
+    def run_website_to_md_js(self, url: str) -> str:
         # Path to your JavaScript file (adjust as needed)
-        js_file_path = './WebsiteToMD/run.js'  
+        js_file_path = "./WebsiteToMD/run.js"
 
         # Construct the command to execute your JavaScript file with Node.js
-        command = ['node', js_file_path, url]
+        command = ["node", js_file_path, url]
 
         try:
-            result = subprocess.run(command, capture_output=True, text=True, check=True)
-            # test if result NoneType object
+            result = subprocess.run(
+                command, capture_output=True, encoding="utf-8", text=True, check=True
+            )
             if result.stdout is None:
                 return self.error_message
             # String cleanup
-            output = result.stdout.strip()  # Remove leading/trailing whitespace
-            
-            #here's a patchy way to remove the Promise { <pending> } string that is returned by the JS file
+            output = str(result.stdout).strip()  # Remove leading/trailing whitespace
+            if url == self.start_urls[0]:
+                print(output)
+
+            # here's a patchy way to remove the Promise { <pending> } string that is returned by the JS file
             if output.startswith("Promise { <pending> }\n"):
-                output = output[len("Promise { <pending> }\n"):]  # Slice off the unwanted part
+                output = output[
+                    len("Promise { <pending> }\n") :
+                ]  # Slice off the unwanted part
             output = self.unmangle_utf8(output)
 
-            # print(output)
+            if url == self.start_urls[0]:
+                print(output)
             return output
 
-        except subprocess.CalledProcessError as e:
+        except:
             return self.error_message
-        
+
     def parse(self, response):
-        links = response.css('a::attr(href)').getall()
-        
-        if response.url not in self.data:
+        links: list = response.css("a::attr(href)").getall()
+
+        # filter for fsu.edu domains and valid links
+        actual_links = []
+        for link in links:
+            if link.startswith("/"):
+                link = f"{self.scheme}://{self.domain}{link}"
+            if (
+                (urlparse(link).netloc == self.domain)
+                and not any(re.search(pattern, link) for pattern in self.deny_patterns)
+                and link not in actual_links
+                and link not in self.start_page_links
+                and link != response.url
+                and self.test_url_validity(link)
+            ):
+                actual_links.append(link)
+        if response.url == self.start_urls[0]:
+            self.start_page_links = actual_links
+        if self.start_afresh or response.url not in self.data:
             markdown_content = str(self.run_website_to_md_js(response.url))
             self.data[response.url] = {}
-            self.data[response.url].update({
-                "links": links,
-                "content": markdown_content,
-            })
-        elif response.url in self.data:
-            if self.data[response.url]["content"] == self.error_message:
-                markdown_content = str(self.run_website_to_md_js(response.url))
-                self.data[response.url].update({
+            self.data[response.url].update(
+                {
+                    "links": actual_links,
                     "content": markdown_content,
-                })
-            if "links" not in self.data[response.url]:
-                self.data[response.url].update({
-                    "links": links,
-                })
-        for link in links:
-                absolute_url = response.urljoin(link)
-                # check if link is within domain of start_url
-                if (
-                    urlparse(absolute_url).netloc 
-                    == urlparse(self.start_urls[0]).netloc
-                ):
-                    yield response.follow(absolute_url, self.parse)
+                }
+            )
+        elif response.url in self.data:
+            if self.update_content or (
+                self.retry_failed_content
+                and self.data[response.url]["content"] == self.error_message
+            ):
+                markdown_content = self.run_website_to_md_js(response.url)
+                self.data[response.url].update(
+                    {
+                        "content": markdown_content,
+                    }
+                )
+            if self.update_links or "links" not in self.data[response.url]:
+                self.data[response.url].update(
+                    {
+                        "links": actual_links,
+                    }
+                )
+        for link in actual_links:
+            yield response.follow(link, self.parse)
+
+
 if __name__ == "__main__":
     spider = GFMDSpider("https://www.fsu.edu")
     spider.run()
--- Spiders/Spider.py
+++ Spiders/Spider.py
@@ -1,9 +1,14 @@
 from sys import argv
 import configparser
+import requests
 import pathlib
 from urllib.parse import urlparse
 import json
+import time
+import re
 
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.signals import spider_closed
@@ -12,9 +17,44 @@ from scrapy.signals import spider_closed
 class Spider(scrapy.Spider):
     name = "Spider"
     start_afresh = False
+    deny_patterns = [
+        # File extensions (case-insensitive, match at the end)
+        re.compile(r".*\.pdf\??.*$", re.IGNORECASE),
+        re.compile(r".*\.docx\??.*$", re.IGNORECASE),
+        re.compile(r".*\.xlsx\??.*$", re.IGNORECASE),
+        re.compile(r".*\.pptx\??.*$", re.IGNORECASE),
+        re.compile(r".*\.jpg\??.*$", re.IGNORECASE),
+        re.compile(r".*\.jpeg\??.*$", re.IGNORECASE),
+        re.compile(r".*\.png\??.*$", re.IGNORECASE),
+        re.compile(r".*\.gif\??.*$", re.IGNORECASE),
+        re.compile(r".*\.svg\??.*$", re.IGNORECASE),
+        re.compile(r".*\.ico\??.*$", re.IGNORECASE),
+        re.compile(r".*\.css\??.*$", re.IGNORECASE),
+        re.compile(r".*\.js\??.*$", re.IGNORECASE),
+        re.compile(r".*\.zip\??.*$", re.IGNORECASE),
+        re.compile(r".*\.xml\??.*$", re.IGNORECASE),
+        re.compile(r".*\.json\??.*$", re.IGNORECASE),
+        re.compile(r".*\.rss\??.*$", re.IGNORECASE),
+        re.compile(r".*\.atom\??.*$", re.IGNORECASE),
+        re.compile(r".*\.rdf\??.*$", re.IGNORECASE),
+        re.compile(r".*\.do\??.*$", re.IGNORECASE),
+        re.compile(r".*\.db\??.*$", re.IGNORECASE),
+        re.compile(
+            r"\?\w\.html\??.*", re.IGNORECASE
+        ),  # matches directory search pages matching ?[letter].html
+        re.compile(
+            r".*mailto:.*", re.IGNORECASE
+        ),  # Matches links starting with "mailto:" (case-insensitive)
+        re.compile(
+            r".*tel:.*", re.IGNORECASE
+        ),  # Matches links starting with "tel:" (case-insensitive)
+    ]
 
+    # Rule = Rule(LinkExtractor(deny=deny_patterns))
     def __init__(self, start_url):
         self.start_urls = [start_url]  # Expect a single start URL
+        self.domain = urlparse(self.start_urls[0]).netloc
+        self.scheme = urlparse(self.start_urls[0]).scheme
         self.error_message = "Page scraping returned no content"
         self.outfile = f"{self.name}_output.json"
         if self.start_afresh:
@@ -24,9 +64,11 @@ class Spider(scrapy.Spider):
                 with open(self.outfile, "r") as infile:
                     self.data = json.load(infile)
             except FileNotFoundError:
-                print(f"File {self.outfile} not found. Cannot progress without existing data. To start fresh, set replace_content to True.")
-            except Exception as e:
-                raise(f"An unexpected error occurred: {e}")
+                print(
+                    f"File {self.outfile} not found. Cannot progress without existing data. To start fresh, set replace_content to True."
+                )
+            except Exception:
+                raise
 
     def parse(self, response):
         # Save data mini example
@@ -44,6 +86,14 @@ class Spider(scrapy.Spider):
             absolute_url = response.urljoin(link)
             if urlparse(absolute_url).netloc == urlparse(self.start_urls[0]).netloc:
                 yield response.follow(absolute_url, self.parse)
+
+    def test_url_validity(self,url)->bool:
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+            return True
+        except:
+            return False
 
     def closed(self, reason):
         # Write accumulated data to a JSON file upon spider closure
@@ -76,7 +126,11 @@ class Spider(scrapy.Spider):
         # Set up crawling process with SpiderClass for a single domain
         process = CrawlerProcess()
         process.crawl(SpiderClass, start_url=self.start_urls[0])
+        start_time = time.time()
         process.start()
+        end_time = time.time()
+        print(f"Time taken: {end_time - start_time} seconds")
+        return self.data
 
 
 if __name__ == "__main__":
